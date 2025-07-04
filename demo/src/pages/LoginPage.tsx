693733877c73ac8caf8e2c6d97463a3627d1d44b
Spencer Wilson <165120193+Swilsondi@users.noreply.github.com>
2025-07-04 09:08:10 -0400

    Moving around files to the right folders to pass tests.

--- demo/src/pages/LoginPage.tsx
+++ demo/src/pages/LoginPage.tsx
@@ -5,6 +5,8 @@ export default function Login() {
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <LoginForm />
+
+        <div className="bg-red-500 text-white p-4">If this is red, Tailwind is working!</div>
       </div>
     </div>
   )
